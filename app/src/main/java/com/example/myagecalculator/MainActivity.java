eeba298bc674e09bfc6202bc0c26dd11181158d7
abhayjat <abhay.jat@outlook.com>
2020-09-23 17:23:47 +0530

    Make User Interface Add Buttons and TextViews

--- app/src/main/java/com/example/myagecalculator/MainActivity.java
+++ app/src/main/java/com/example/myagecalculator/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.myagecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btndob,btndate,calculate;
+    TextView result;
+    DatePickerDialog.OnDateSetListener dateSetListener1,dateSetListener2;   //The listener used to indicate the user has finished selecting a date
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
